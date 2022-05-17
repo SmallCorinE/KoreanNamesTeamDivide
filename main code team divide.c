2239980aa180360d898b38c4dddf05896a0574cf
SmallCorinE <qqqqmmss11@gmail.com>
2022-05-18 08:09:01 +0900

    first do
    yeah

---
+++ main code team divide.c
@@ -0,0 +1,54 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    char TeamOne[7][11] =
+    {
+    "철수",
+    "영희",
+    "콩쥐",
+    "팥쥐",
+    "놀부",
+    "흥부",
+    "제비"
+    } ;
+
+    int head_number, letters;
+    head_number = sizeof(TeamOne) / sizeof(TeamOne[0]);
+    letters = sizeof(TeamOne[0]) / sizeof(char);
+
+    int i, j, k, x;
+    char jojjagi[7][11];
+    srand(time(NULL));
+
+    for (i = 0; i < head_number; i++)
+    {
+        again:;
+        x = rand() % head_number;
+        for (j = 0; j < letters; j++)
+            {
+            jojjagi[i][j] = TeamOne[x][j];
+            }
+        if (i != 0)
+            {
+            for (k = 0; k < i; k++)
+                {
+                if (jojjagi[i][0] == jojjagi[k][0])
+                goto again;
+                }
+            }
+    }
+printf("\n한글이름을 가진 멤버들을 4명 단위로 나누는 코드\n\n");
+
+    for (i = 0; i < head_number; i++)
+    {
+        if (i % 4 == 0)
+            {
+            printf("\n%d번째 조\n", (i / 4) + 1);
+            }
+        printf("%s\n", &jojjagi[i][0]);
+    }
+}
